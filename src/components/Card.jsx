2dbbf67f10986d3a1c8a570a3c1d40732926d40c
Vishesh Goyal <146622892+Vishesh1207-g@users.noreply.github.com>
2025-06-28 20:41:09 +0530

    Update Card.jsx

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
 import { useFirebase } from "../context/Firebase";
-
+ 
 const BookCard = (props) => {
   const firebase = useFirebase();
   const navigate = useNavigate();
